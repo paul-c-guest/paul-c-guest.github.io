d3a2c244c491ebba5c7a64bb8b07ba142faace6e
Paul Guest <paul.christopher.guest@gmail.com>
2023-07-19 17:17:37 +1200

    refactor hardcoded innerHTML to document methods

--- bread-calc/main.js
+++ bread-calc/main.js
@@ -1,15 +1,32 @@
 const flourBlock = document.getElementById('flour-block')
 
-const html =
-  'Flour: <input class="flour-name" placeholder="wholemeal"> Amount: <input class="flour-amount" type="number" placeholder="grams" />'
-
 const addFlour = () => {
   const newLine = document.createElement('div')
-  newLine.innerHTML = html
+
+  const flourInput = document.createElement('input')
+  flourInput.classList.add('flour-name')
+  flourInput.setAttribute('placeholder', 'wholemeal')
+
+  const amountInput = document.createElement('input')
+  amountInput.classList.add('flour-amount')
+  amountInput.setAttribute('placeholder', 'grams')
+  amountInput.setAttribute('type', 'number')
+
+  const flourTitle = document.createElement('span')
+  flourTitle.innerHTML = 'Flour: '
+
+  const amountTitle = document.createElement('span')
+  amountTitle.innerHTML = 'Amount: '
+
+  newLine.appendChild(flourTitle)
+  newLine.appendChild(flourInput)
+  newLine.appendChild(amountTitle)
+  newLine.appendChild(amountInput)
+
   flourBlock.appendChild(newLine)
 }
 
-document.getElementById('newline').onclick = (e) => addFlour()
-
 // create one initial line by default
 addFlour()
+
+document.getElementById('newline').onclick = (e) => addFlour()
