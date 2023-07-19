a055aee1f547bc1134829dbe38a7f7a811fa030b
Paul Guest <paul.christopher.guest@gmail.com>
2023-07-19 17:23:58 +1200

    add hydration input element

--- bread-calc/main.js
+++ bread-calc/main.js
@@ -4,24 +4,36 @@ const addFlour = () => {
   const newLine = document.createElement('div')
 
   const flourInput = document.createElement('input')
-  flourInput.classList.add('flour-name')
+  flourInput.classList.add('flour-entry-name')
   flourInput.setAttribute('placeholder', 'wholemeal')
 
   const amountInput = document.createElement('input')
-  amountInput.classList.add('flour-amount')
+  amountInput.classList.add('flour-entry-number')
   amountInput.setAttribute('placeholder', 'grams')
   amountInput.setAttribute('type', 'number')
 
+  const hydrationInput = document.createElement('input')
+  hydrationInput.classList.add('flour-entry-number')
+  hydrationInput.setAttribute('placeholder', 'wet%')
+  hydrationInput.setAttribute('type', 'number')
+
   const flourTitle = document.createElement('span')
-  flourTitle.innerHTML = 'Flour: '
+  flourTitle.innerHTML = 'Flour:'
 
   const amountTitle = document.createElement('span')
-  amountTitle.innerHTML = 'Amount: '
+  amountTitle.innerHTML = 'Amount:'
+
+  const hydrationTitle = document.createElement('span')
+  hydrationTitle.innerHTML = 'Hydration:'
 
   newLine.appendChild(flourTitle)
   newLine.appendChild(flourInput)
+
   newLine.appendChild(amountTitle)
   newLine.appendChild(amountInput)
+
+  newLine.appendChild(hydrationTitle)
+  newLine.appendChild(hydrationInput)
 
   flourBlock.appendChild(newLine)
 }
