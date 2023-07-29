c3aa6f7d90d3abf0cd1bf50a4d9386d06df5cad4
Paul Guest <paul.christopher.guest@gmail.com>
2023-07-30 09:04:41 +1200

    create basic flours array for testing and development

---
+++ bread-calc/flours.js
@@ -0,0 +1,9 @@
+export const flours = [
+  { name: 'Wheat - High Grade', defHydration: 73 },
+  { name: 'Wheat - Regular', defHydration: 68 },
+  { name: 'Wheat - Wholemeal', defHydration: 65 },
+  { name: 'Rye', defHydration: 67 },
+  { name: 'Rye - Wholemeal', defHydration: 63 },
+  { name: 'Spelt', defHydration: 62 },
+  { name: 'Spelt - Wholemeal', defHydration: 59 },
+]
