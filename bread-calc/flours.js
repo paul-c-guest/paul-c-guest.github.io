716c37f8ff98d3cfb7b7c43cfa8fcbc80e73d521
Paul Guest <paul.christopher.guest@gmail.com>
2024-02-10 14:05:48 +1300

    rearrange folder structure
    dev academy foundations blog moved away from root -> da-foundations-blog folder
    reinstated structure before dev academy: placeholder main site page, screamer subfolder for publicly available privacy policy

--- bread-calc/flours.js
+++
@@ -1,9 +0,0 @@
-export default [
-  { name: 'Wheat - High Grade', defHydration: 73 },
-  { name: 'Wheat - Regular', defHydration: 68 },
-  { name: 'Wheat - Wholemeal', defHydration: 65 },
-  { name: 'Rye', defHydration: 67 },
-  { name: 'Rye - Wholemeal', defHydration: 63 },
-  { name: 'Spelt', defHydration: 62 },
-  { name: 'Spelt - Wholemeal', defHydration: 59 },
-]
